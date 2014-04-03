3fc599b112f163ccdf4d8eec0f35295c17e2efc0
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2014-04-03 17:52:10 +0200

    Add necessary methods. No implementation yet :)

--- E3.java
+++ E3.java
@@ -8,6 +8,8 @@ import org.rlcommunity.rlglue.codec.types.*;
  */
 public class E3 {
 
+    public double discount;
+
     // Transition probabilities in the MDP
     private TransitionProbabilities tps;
 
@@ -18,10 +20,35 @@ public class E3 {
     // State -> Total reward
     private Map<Observation, Integer> sr;
 
+    // Current policy
+    private Map<Observation, Action> currentPolicy;
+
     public E3() {
         tps = new TransitionProbabilities();
         sasv = new HashMap<>();
         sr = new HashMap<>();
+    }
+
+    /**
+     * Find the next action.
+     */
+    public Action nextAction(Observation state) {
+        if (!isKnown(state)) {
+            // Unknown state, resume balanced wandering
+            currentPolicy = null;
+            return balancedWandering(state);
+        }
+
+        if (currentPolicy == null) {
+            // Start exploitation/exploration
+            currentPolicy = findExplorationPolicy(state);
+
+            if (shouldExploit(currentPolicy, state)) {
+                currentPolicy = findExploitationPolicy(state);
+            }
+        }
+
+        return currentPolicy.get(state);
     }
 
     /**
@@ -43,6 +70,45 @@ public class E3 {
         updateReward(from, action, to, reward);
     }
 
+    /**
+     * Find the balanced wandering action
+     */
+    private Action balancedWandering(Observation state) {
+        return null;
+    }
+
+    /**
+     * Is a state known?
+     * TODO: this :)
+     */
+    private boolean isKnown(Observation state) {
+        return getVisits(state) > 100;
+    }
+
+    /**
+     * Should we exploit? Or is exploration beneficial?
+     */
+    private boolean shouldExploit(Map<Observation, Action> policy, Observation state) {
+        return false;
+    }
+
+    // Functions for policy calculations {{{
+
+    private Map<Observation, Action> findExplorationPolicy(Observation state) {
+        return null;
+    }
+
+    private Map<Observation, Action> findExploitationPolicy(Observation state) {
+        return null;
+    }
+
+    // }}}
+
+    // Functions for updating after an observation {{{
+
+    /**
+     * Update visits statistics
+     */
     private void updateVisits(Observation from, Action action, Observation to) {
         Map<Action, Map<Observation, Integer>> asv = sasv.get(from);
         if (asv == null) {
@@ -94,6 +160,10 @@ public class E3 {
         sr.put(to, r);
     }
 
+    // }}}
+
+    // Functions for getting visit statistics {{{
+
     private Integer getVisits(Observation from) {
         Integer visits = 0;
         for (Action action : sasv.get(from).keySet()) {
@@ -111,5 +181,7 @@ public class E3 {
     private Integer getVisits(Observation from, Action action, Observation to) {
         return sasv.get(from).get(action).get(to);
     }
+
+    // }}}
 
 }
